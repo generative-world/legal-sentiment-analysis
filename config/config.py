b15d1810cc6ca749f97c10bb7f6d123758eb7139
generative-world <generative.customai@gmail.com>
2025-03-31 12:03:25 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -4,10 +4,14 @@ import yaml
 CONFIG_PATH = "config/config.yml"
 
 def load_config():
-    with open(CONFIG_PATH, "r") as file:
-        return yaml.safe_load(file)
+    try:
+        with open(CONFIG_PATH, "r") as file:
+            return yaml.safe_load(file)
+    except FileNotFoundError:
+        raise Exception("Configuration file not found!")
+    except yaml.YAMLError as e:
+        raise Exception(f"Error parsing YAML file: {e}")
 
 config = load_config()
-
-DB_CONFIG = config["DB"]
-AWS_CONFIG = config["AWS"]
+DB_CONFIG = config.get("DB", {})
+AWS_CONFIG = config.get("AWS", {})
